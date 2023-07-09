85d3f0668a620a445f04ce62ceb0052d67a07f80
zacktao <zacktao24@gmail.com>
2023-07-09 10:03:31 +0800

    update

--- bacup.py
+++ bacup.py
@@ -1,3 +1,6 @@
 import os
 
-os.popen('f: && cd F:\BaiduSyncdisk\投资\app\investment_analysis&& git add . && git commit --message update && git push').readlines()
+result = ','.join(os.popen('f: && cd F:/BaiduSyncdisk/投资/app/investment_analysis&& git add . && git commit --message update && git push').readlines())
+
+with open(r'F:\BaiduSyncdisk\投资' + '/logs.txt', 'w') as f:
+    f.write(result)
